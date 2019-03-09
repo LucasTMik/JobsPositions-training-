0adc2f3f91b012490c4712cbd4b753fbedf42c81
Lucas Cerqueira Cunha <lucascercun@gmail.com>
2019-03-09 17:39:33 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './stylesheet/App.css';
+
+import Jobs from './components/jobs';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className={"container"}>
+        <div className={"headerContainer"}>
+          <div>
+            <h1>Job Positions</h1>
+            <h6>Vagas de trabalho relacionadas a Tecnologia</h6>
+          </div>
+        </div>
+        <Jobs />
+        <button className={"floaingButton"}>+</button>
       </div>
     );
   }
---
+++ src/components/jobs.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+import '../stylesheet/jobs.css';
+
+
+export default class Jobs extends Component {
+    render() {
+        return (
+            <div className={"content"}>
+                <div className={"searchContainer"}>
+                    <h3>Busque sua vaga</h3>
+                    <hr />
+                </div>
+                <div className={"jobsContainer"}>
+                    <input placeholder={"Nome da vaga..."} />
+                    <div>
+                        <select >
+                            <option selected disabled>Estado</option>
+                            <option >test</option>
+                        </select>
+                        <button>Buscar</button>
+                    </div>
+                </div>
+            </div> 
+        );
+    }
+}
+
